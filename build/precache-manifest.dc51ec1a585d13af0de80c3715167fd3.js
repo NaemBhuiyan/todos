9566a6bc9428b5b44252b4c4d1494e8d33349702
Naem Bhuiyan <hopingboynaem@gmail.com>
2020-11-02 14:38:30 +0600

    post

---
+++ build/precache-manifest.dc51ec1a585d13af0de80c3715167fd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27fef4d1aa5a8074c6b7c52e55cb7040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "235311de724726bb9462",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "39fb5cde7442ce76eaac",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "235311de724726bb9462",
+    "url": "/static/js/2.1e5bd45c.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.1e5bd45c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39fb5cde7442ce76eaac",
+    "url": "/static/js/main.f7d2b01f.chunk.js"
+  },
+  {
+    "revision": "55c65d67cb19f699ec3f",
+    "url": "/static/js/runtime-main.18ca64aa.js"
+  }
+]);
